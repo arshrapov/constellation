129c06bdb080cc4927534c231e2fdb7a7f7fc064
Raphael <ashrapov.rafa@yandex.com>
2020-09-23 16:54:19 +0500

    Initial commit

---
+++ System/__init__.py
@@ -0,0 +1,6 @@
+from .draw import *
+from .input import *
+from .model import *
+from .system_objects import *
+from .extra_objects import *
+
---
+++ main.py
@@ -0,0 +1,9 @@
+import System
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
